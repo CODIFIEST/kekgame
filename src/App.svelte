fed63ac88ce47c93b711fdaab6c5fc9eeb617421
CODIFIEST <surfdean808@gmail.com>
2023-05-07 10:07:01 -0700

    moved div to app.svelte from index.html

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,13 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Kek Game</h1>
 
-
-  <p>
-    Check out <a href="https://kek.cash">Kek.CASH</a>
-  </p>
-
+  <div id="game"></div>
   
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
--- src/sounds/playDeathSound.js
+++
@@ -1,5 +0,0 @@
-function playDeathSound() {
-    let ugh = document.getElementById("sound-ugh")
-    ugh.play();
-}
-module.exports = playDeathSound;
